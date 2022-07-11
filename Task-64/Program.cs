977a348f05d6497a7d945cb847905fb3cfa38463
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-07-11 22:27:04 +0300

    Решение задачи 64

--- Task-64/Program.cs
+++ Task-64/Program.cs
@@ -3,3 +3,19 @@
 // M = 1; N = 5. ->120
 // M = 4; N = 6. -> 120
 
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите М: ");
+int m = Convert.ToInt32(ReadLine());
+Write("Введите N: ");
+int n = Convert.ToInt32(ReadLine());
+
+WriteLine($"{MultiplicatNaturalNumbers(m, n)}");
+
+int MultiplicatNaturalNumbers(int numberOne, int numberTwo)
+{
+   if (numberTwo > numberOne) return numberOne * MultiplicatNaturalNumbers(numberOne + 1, numberTwo);
+   return numberOne;
+}
